ca48e4bbc7e53634d1f28a915f3b2e47cd17fc50
Chanagun Viriyasathapornpong <chanagun.vir@gmail.com>
2023-08-23 21:09:10 +0700

    guncv

--- Homework/Week3/vaccine-book-app/src/components/banner.tsx
+++ Homework/Week3/vaccine-book-app/src/components/banner.tsx
@@ -4,14 +4,15 @@ import styles from "@/components/banner.module.css"
 export default function Banner(){
     return (
         <div className={styles.banner}>
-            <Image src="/img/vaccine.jpg"
+            <Image src="/img/vaccine.webp"
             alt="banner"
             fill={true}
             priority
             objectFit="cover"
             />
             <div className={styles.bannerText}>
-                <h1>ฉีดวัคซีนป้องกันโรคร้าย</h1>
+                <h1>บริการฉีดวัคซีนฟรี</h1>
+                <h2>ฉีดวัคซีนป้องกันโรคร้ายอย่างดีและทั่วถึว</h2>
             </div>
         </div>
     )
